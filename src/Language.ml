9c9b1f7f91826670c8cb485a00cde10124535f73
Serezha Karpunin <serezha.karpunin@gmail.com>
2019-03-28 16:00:32 +0300

    HW6

--- src/Language.ml
+++ src/Language.ml
@@ -56,6 +56,8 @@ module Expr =
         *, /, %              --- multiplication, division, reminder
     *)
       
+    let update x v s = fun y -> if x = y then v else s y
+	  
     (* Expression evaluator
 
           val eval : state -> t -> int
@@ -150,11 +152,52 @@ module Stmt =
 
        which returns a list of formal parameters and a body for given definition
     *)
-    let rec eval _ = failwith "Not Implemented Yet"
+    let rec eval env (s, i, o) t =
+        match t with
+            | Read    v       -> (State.update v (List.hd i) s, List.tl i, o)
+            | Write   e       -> (s, i, o @ [Expr.eval s e])
+            | Assign (v, e)   -> (State.update v (Expr.eval s e) s, i, o)
+            | Seq    (e1, e2) ->
+                let stmt = eval env (s, i, o) e1
+                in eval env stmt e2
+            | Skip            -> (s, i, o)
+            | If (e1, e2, e3) ->
+                if (Expr.eval s e1) != 0 then eval env (s, i, o) e2 else eval env (s, i, o) e3
+            | While (e1, e2)  ->
+                let r = Expr.eval s e1 in
+                if r != 0 then eval env (eval env (s, i, o) e2) (While (e1, e2)) else (s, i, o)
+            | Repeat (e1, e2)  ->
+                let (s, i, o) = eval env (s, i, o) e1 in
+                let r = Expr.eval s e2 in
+                if r != 0 then (s, i ,o) else eval env (s, i, o) t
+            | Call (name, args) ->
+                let (arg_names, locals, body) = env#definition name in
+                let args = List.combine arg_names (List.map (Expr.eval s) args) in
+                let state = State.push_scope s (arg_names @ locals) in
+                let fun_env_w_args = List.fold_left (fun s (name, value) -> State.update name value s) state args in
+                let (new_s, i, o) = eval env (fun_env_w_args, i, o) body in
+                (State.drop_scope new_s s, i, o)
                                 
     (* Statement parser *)
     ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
+       line:
+        "read" "(" x:IDENT ")"         {Read x}
+        | "write" "(" e:!(Expr.parse) ")" {Write e}
+        | x:IDENT ":=" e:!(Expr.parse)    {Assign (x, e)}
+        | "if" e1:!(Expr.parse) "then" e2:parse "else" e3:parse "fi" {If (e1, e2, e3)}
+        | "if" e1:!(Expr.parse) "then" e2:parse "fi" {If (e1, e2, Skip)}
+        | "if" e1:!(Expr.parse) "then" e2:parse e3:elif {If (e1, e2, e3)}
+        | "skip" {Skip}
+        | "while" e1:!(Expr.parse) "do" e2:parse "od" {While (e1, e2)}
+        | "repeat" e1:parse "until" e2:!(Expr.parse) {Repeat (e1, e2)}
+        | "for" e1:parse "," e2:!(Expr.parse) "," e3:parse "do" s:parse "od" {Seq (e1, While (e2, Seq(s, e3)))}
+        | name:IDENT "(" args:(!(Expr.parse))* ")" {Call (name, args)};
+      parse:
+        l:line ";" rest:parse {Seq (l, rest)} | line;
+      elif:
+        "elif" e1:!(Expr.parse) "then" e2:parse "else" e3:parse "fi" {If (e1, e2, e3)}
+        | "elif" e1:!(Expr.parse) "then" e2:parse "fi" {If (e1, e2, Skip)}
+        | "elif" e1:!(Expr.parse) "then" e2:parse e3:elif {If (e1, e2, e3)}
     )
       
   end
@@ -167,7 +210,13 @@ module Definition =
     type t = string * (string list * string list * Stmt.t)
 
     ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
+      parse: "fun" name:IDENT "(" args:(IDENT)* ")" local:(%"local" (IDENT)*)? "{" body:!(Stmt.parse) "}"
+        {
+            let local = match local with
+            | Some x -> x
+            | _ -> [] in
+            name, (args, local, body)
+        }
     )
 
   end
